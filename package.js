f080ebb0693ab61dbd82837b3a83a70bff66a112
maxim <uzumaxy@gmail.com>
2015-06-04 15:49:00 +0300

    Fixed bug.

--- package.js
+++ package.js
@@ -5,57 +5,51 @@ Package.describe({
   git: 'https://github.com/JSSolutions/meteor-flag-icon'
 });
 
+var flags = [
+  'ad', 'ae', 'af', 'ag', 'ai', 'al', 'am', 'ao', 'aq', 'ar', 'as', 'at', 'au',
+  'aw', 'ax', 'az', 'ba', 'bb', 'bd', 'be', 'bf', 'bg', 'bh', 'bi', 'bj', 'bl',
+  'bm', 'bn', 'bo', 'bq', 'br', 'bs', 'bt', 'bv', 'bw', 'by', 'bz', 'ca', 'cc',
+  'cd', 'cf', 'cg', 'ch', 'ci', 'ck', 'cl', 'cm', 'cn', 'co', 'cr', 'cu', 'cv',
+  'cw', 'cx', 'cy', 'cz', 'de', 'dj', 'dk', 'dm', 'do', 'dz', 'ec', 'ee', 'eg',
+  'eh', 'er', 'es', 'et', 'fi', 'fj', 'fk', 'fm', 'fo', 'fr', 'ga', 'gb', 'gd',
+  'ge', 'gf', 'gg', 'gh', 'gi', 'gl', 'gm', 'gn', 'gp', 'gq', 'gr', 'gs', 'gt',
+  'gu', 'gw', 'gy', 'hk', 'hm', 'hn', 'hr', 'ht', 'hu', 'id', 'ie', 'il', 'im',
+  'in', 'io', 'iq', 'ir', 'is', 'it', 'je', 'jm', 'jo', 'jp', 'ke', 'kg', 'kh',
+  'ki', 'km', 'kn', 'kp', 'kr', 'kw', 'ky', 'kz', 'la', 'lb', 'lc', 'li', 'lk',
+  'lr', 'ls', 'lt', 'lu', 'lv', 'ly', 'ma', 'mc', 'md', 'me', 'mf', 'mg', 'mh',
+  'mk', 'ml', 'mm', 'mn', 'mo', 'mp', 'mq', 'mr', 'ms', 'mt', 'mu', 'mv', 'mw',
+  'mx', 'my', 'mz', 'na', 'nc', 'ne', 'nf', 'ng', 'ni', 'nl', 'no', 'np', 'nr',
+  'nu', 'nz', 'om', 'pa', 'pe', 'pf', 'pg', 'ph', 'pk', 'pl', 'pm', 'pn', 'pr',
+  'ps', 'pt', 'pw', 'py', 'qa', 're', 'ro', 'rs', 'ru', 'rw', 'sa', 'sb', 'sc',
+  'sd', 'se', 'sg', 'sh', 'si', 'sj', 'sk', 'sl', 'sm', 'sn', 'so', 'sr', 'ss',
+  'st', 'sv', 'sx', 'sy', 'sz', 'tc', 'td', 'tf', 'tg', 'th', 'tj', 'tk', 'tl',
+  'tm', 'tn', 'to', 'tr', 'tt', 'tv', 'tw', 'tz', 'ua', 'ug', 'um', 'us', 'uy',
+  'uz', 'va', 'vc', 've', 'vg', 'vi', 'vn', 'vu', 'wf', 'ws', 'ye', 'yt', 'za',
+  'zm', 'zw'
+];
 
-addDirectory = function () {
-  if (!arguments.length) {
-    throw 'You must specify directory name!'
-  }
-  var _ = Npm.require('underscore');
-  var path = Npm.require('path');
-  var fs = Npm.require('fs');
-
-  var dir = arguments[0];
-  var otherArgs = _.rest(arguments);
-  // XXX COMPAT WITH 0.8.x
-  var addFiles = this.addFiles || this.add_files;
-
-
-  var srcPath;
-  if (process.env.PACKAGE_DIRS) {
-    srcPath = process.env.PACKAGE_DIRS;
-  } else {
-    srcPath = path.join(
-      path.resolve('.'), 'packages'
-    );
-  }
-  srcPath = path.join(srcPath, dir);
-  var files = fs.readdirSync(srcPath);
-  files.forEach(function (file) {
-    file = path.join(dir, file);
-    var args = [file].concat(otherArgs);
-    addFiles.apply(this, args);
-  });
+var path = Npm.require('path');
+var basePath = 'flags';
+var flagsFilesGetter = function (srcPath) {
+  srcPath = path.join(basePath, srcPath);
+  return function () {
+    var files = [];
+    flags.forEach(function (flagFile) {
+      flagFile = path.join(srcPath, flagFile + '.svg');
+      files.push(flagFile);
+    });
+    return files;
+  };
 };
-
+var getSquareFlags = flagsFilesGetter('1x1');
+var getWideFlags = flagsFilesGetter('4x3');
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.constructor.prototype.addDirectory = addDirectory;
-
-  var path = Npm.require('path');
-  api.addDirectory(path.join(
-    'flag-icon', 'flags', '1x1'
-  ), [
-    'client'
-  ],{
-    isAsset: true
-  });
-  api.addDirectory(path.join(
-    'flag-icon', 'flags', '4x3'
-  ), [
-    'client'
-  ],{
-    isAsset: true
-  });
   api.addFiles('flag-icon.css', 'client');
+  [getSquareFlags(), getWideFlags()].forEach(function (files) {
+    files.forEach(function (file) {
+      api.addFiles(file, 'client', {isAsset: true});
+    });
+  });
 });
